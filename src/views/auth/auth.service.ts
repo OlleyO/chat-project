380e4802739916eebf97fa62f54ce43a9068aec1
Oleh <my.mail.olech@gmail.com>
2023-06-13 21:40:14 +0300

    env

--- src/views/auth/auth.service.ts
+++ src/views/auth/auth.service.ts
@@ -51,7 +51,7 @@ class AuthService {
 
   async sendPasswordResetEmail (email: string) {
     const { error } = await useSupabase().auth.resetPasswordForEmail(email, {
-      redirectTo: `${import.meta.env.VITE_APP_BASE_URL_DEV}/auth/reset-password`
+      redirectTo: `${import.meta.env.MODE === 'development' ? import.meta.env.VITE_APP_BASE_URL_DEV : import.meta.env.VITE_APP_BASE_URL_PROD}/auth/reset-password`
     })
 
     if (error) {
